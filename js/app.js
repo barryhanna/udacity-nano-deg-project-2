2e88933f48965b5a9676c84490b72962045f8fcb
Barry Hanna <barryahanna@gmail.com>
2018-09-17 14:05:38 +0100

    Adding moves panel updates

--- js/app.js
+++ js/app.js
@@ -10,7 +10,8 @@ const DIAMOND = "fa-diamond",
       BICYCLE = "fa-bicycle",
       LEAF    = "fa-leaf",
       BOMB    = "fa-bomb";
-      
+
+var openCards = [];
 
 var deck = [DIAMOND, PLANE, ANCHOR, BOLT, CUBE, BICYCLE, LEAF, BOMB,
             DIAMOND, PLANE, ANCHOR, BOLT, CUBE, BICYCLE, LEAF, BOMB];
@@ -22,22 +23,32 @@ var deck = [DIAMOND, PLANE, ANCHOR, BOLT, CUBE, BICYCLE, LEAF, BOMB,
  *   - add each card's HTML to the page
  */
 
-
 function generateDeck() {
     deck = shuffle(deck);
     let deckDisplay = document.getElementsByClassName("deck")[0];
     let deckHTML = "";
 
+    // remove current cards.
     while(deckDisplay.firstChild) {
         deckDisplay.removeChild(deckDisplay.firstChild);
     }
 
+    // generate li items from shuffled deck
     for(const card of deck) {
         deckHTML += `<li class="card"><i class="fa ${card}"></i></li>`;
     }
 
+    // update DOM
     deckDisplay.innerHTML = deckHTML;
+    
     addCardEventListeners();
+    
+    // reset 3 moves and stars
+    document.getElementsByClassName("moves")[0].innerText = 3;
+    const stars = `<li><i class="fa fa-star"></i></li>
+                   <li><i class="fa fa-star"></i></li>
+                   <li><i class="fa fa-star"></i></li>`;
+    document.getElementsByClassName("stars")[0].innerHTML = stars;
 }
 
 generateDeck();
@@ -68,20 +79,88 @@ document.getElementsByClassName("restart")[0].addEventListener("click", generate
 function addCardEventListeners() {
     let cards = document.querySelectorAll(".card");
     for(const card of cards) {
-        console.log("Click event: " + card);
         card.addEventListener("click", cardClick);
     }
 }
 
 function cardClick(event) {
-    console.log("Click event: " + event.target);
     event.target.classList.add("open");
     event.target.classList.add("show");
+    checkMatch(event.target);
 }
 
  /*  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
  *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ */
+
+function hideOpenCards(card) {
+    for(card of openCards) {
+        card.classList.remove("show");
+        card.classList.remove("open");
+    }
+}
+
+function checkMatch(card) {
+    // if there are no cards on the open list,
+    // return false after adding the open card to the list.
+    const cardTemplate = `${card.firstChild.getAttribute("class")} added to open card list.`;
+    if(openCards.length < 1) {
+        addToOpenCardList(card);
+        console.log(cardTemplate);
+    } else {
+        // if the same card is clicked then just ignore.
+        if(openCards[0] === card) {
+            return;
+        }
+        addToOpenCardList(card);
+        console.log(cardTemplate);
+        if(openCards[0].firstChild.getAttribute("class") ===                        openCards[1].firstChild.getAttribute("class")) {
+            console.log("Match");
+            cardsMatched();
+            removeMatchedEventListeners();
+            clearOpenCardList();
+            return true;
+        } else {
+            console.log("No match");
+            hideOpenCards(card);
+            clearOpenCardList();
+            removeMoveStar();
+            return false;
+        }
+    }
+}
+
+function removeMatchedEventListeners() {
+    openCards[0].removeEventListener("click", cardClick);
+    openCards[1].removeEventListener("click", cardClick);
+}
+
+function clearOpenCardList() {
+    while(openCards.pop()) { }
+}
+
+function addToOpenCardList(card) {
+    openCards.push(card);
+}
+
+function cardsMatched() {
+    openCards[0].classList.add("match");
+    openCards[1].classList.add("match");
+}
+
+function removeMoveStar() {
+    const starList = document.getElementsByClassName("stars")[0];
+    // don't remove if there are no more moves.
+    if(starList.childElementCount < 1) { return; }
+    starList.removeChild(starList.firstElementChild);
+    
+    let moves = document.getElementsByClassName("moves")[0].innerText.toString();
+    moves -= 1;
+    document.getElementsByClassName("moves")[0].innerText = moves;
+}
+
+ 
+/* *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
